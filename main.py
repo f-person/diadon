52dac28bb1fe3584fb1d2215a8f2629cf7f6eeee
f-person <agakaryan.arshak@gmail.com>
2019-02-23 16:06:13 +0400

    +posting on diasp and mast

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from mastodon import Mastodon
-import keys
+from keys import *
+import sys, os
+import diaspy, json
 
-api = Mastodon(keys.client_key, keys.client_secret, keys.access_token, api_base_url="https://xn--69aa8bzb.xn--y9a3aq")
-api.toot("python-ով թթելու փորձ")
+given_args = sys.argv[1:]
+args = ['-d', '--diaspora', '-m', '--mastodon', '-h', '--help', 'config', 'mastodon']
+post = ''
+str_post =''
+for argnum, arg in enumerate(given_args):
+    if arg not in args:
+        post = ' '.join(given_args[argnum:])
+        if len(post)==0:
+            print("the post is empty")
+        if len(post)<200:
+            api = Mastodon(m_keys.client_key, m_keys.client_secret, m_keys.access_token, m_keys.pod)
+            # api.toot(post)
+            print('successfully tooted on mastodon')
+        else:
+            api = diaspy.connection.Connection(pod = d_keys.pod, username=d_keys.username, password = d_keys.password)
+            api.login()
+            # diaspy.streams.Stream(api).post(post)
+            print('successfully shared on diaspora')
+        break
+
+
+# if '-d' in sys.argv:
+#     if ()
+#     print('shared on diaspora')
+# else if '-m' in sys.argv:
+#     print('shared on mastodon')
+
+
+# api = Mastodon(m_keys.client_key, m_keys.client_secret, m_keys.access_token, api_base_url="https://xn--69aa8bzb.xn--y9a3aq")
+# api.toot("")
