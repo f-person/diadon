3957221ed7600d8bc04646fca0f6443ca2ee6408
f-person <agakaryan.arshak@gmail.com>
2019-02-23 21:55:43 +0400

    added help message and semi-done

--- main.py
+++ main.py
@@ -7,8 +7,29 @@ given_args = sys.argv[1:]
 args = ['-d', '--diaspora', '-m', '--mastodon', '-h', '--help', 'config', 'mastodon']
 post = ''
 
+keys = json.load(open("keys.json", "r"))
+mastodonMax = keys["mastodonMax"]
+
+help_message = """USAGE: 
+    just type diadon <your text here> to share it on diaspora if the length of the text is more than length for tooting on mastodon. by default it's set to 140.
+
+    to override max length and share on diaspora add -d or --diaspora argument (diadon -d <your text here>)
+    to override max lentgh and toot on mastodon add -m or --mastodon argument (diadon -m <your text here>)
+
+FIRST TIME USE:
+    first of all you need to create a new mastodon application in <your pod address>/settings/applications/new 
+    then you have to configure mastodon and diaspora accounts
+
+CONFIGURATIN:
+    to change diaspora account settings type: diadon config <pod address> <username> <password>
+    to change mastodon account settings type: diadon config <pod address> <client_secret> <access_token> <client_key> (if you dont have them get them by following 'FIRST TIME USE')
+        """
 
 for argnum, arg in enumerate(given_args):
+    if arg == '-h' or arg == '--help':
+        print(help_message)
+        break
+
     if arg == 'config':
         if given_args[argnum+1] == '-m' or given_args[argnum+1]=='--mastodon':
             with open("keys.json", "r+") as jsonFile:
@@ -20,6 +41,7 @@ for argnum, arg in enumerate(given_args):
                     data["m_keys"]["client_key"] = given_args[argnum+5]
                 except:
                     print("please enter all keys")
+                    break
                 jsonFile.seek(0)
                 json.dump(data, jsonFile)
                 jsonFile.truncate()
@@ -33,6 +55,7 @@ for argnum, arg in enumerate(given_args):
                     data["d_keys"]["password"] = given_args[argnum+4]
                 except:
                     print("please enter all keys")
+                    break
                 jsonFile.seek(0)
                 json.dump(data, jsonFile)
                 jsonFile.truncate()
@@ -47,13 +70,12 @@ for argnum, arg in enumerate(given_args):
             post = ' '.join(given_args[argnum+1:])
         if len(post)==0:
             print("the post is empty")
-        # else if len(post)<200 or arg == '-m' or arg == '--mastodon':
-        elif len(post)<200 and arg != '-d' and arg != '--diaspora':
-            api = Mastodon(m_keys.client_key, m_keys.client_secret, m_keys.access_token, m_keys.pod)
+        elif len(post)<mastodonMax and arg != '-d' and arg != '--diaspora':
+            api = Mastodon(keys["m_keys"]["client_key"], keys["m_keys"]["client_secret"], keys["m_keys"]["access_token"], keys["m_keys"]["pod"])
             # api.toot(post)
             print('successfully tooted on mastodon')
         else:
-            api = diaspy.connection.Connection(pod = d_keys.pod, username=d_keys.username, password = d_keys.password)
+            api = diaspy.connection.Connection(pod = keys["d_keys"]["pod"], username=keys["d_keys"]["username"], password = keys["d_keys"]["password"])
             api.login()
             # diaspy.streams.Stream(api).post(post)
             print('successfully shared on diaspora')
